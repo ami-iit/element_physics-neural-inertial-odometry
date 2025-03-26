17a8968b90adfbf66e948eb45a8365de42527def
Cheng Guo <gcleon96@gmail.com>
2025-03-26 15:16:46 +0100

    update EKF State object class

--- EKF/states.py
+++ EKF/states.py
@@ -1,4 +1,7 @@
 r"""Define the EKF states."""
+from utils import math_utils as maths
+import numpy as np
+
 class States(object):
     def __init__(self):
         super(States, self).__init__()
@@ -61,7 +64,91 @@ class States(object):
             f"  ba:\n{repr(self.s_ba) if self.s_ba is not None else 'Not initialized'}\n"
             f")"
         )
+    
+    def update_state_with_corrections(self, dX):
+        """
+        Given the delta_X obtained with K(h(X)-nn_output), 
+        return the updated state vector X = X + dX.
+        """
+        dX_past = dX[:-15] # N-steps past states of dim 6N
+        dX_evol = dX[-15:] # current state of dim 15
+        assert dX_past.flatten().shape[0] == (self.N * 6), (
+            f"Number of past error states {dX_past.flatten().shape[0]} does not match the number of states {self.N * 6} in the filter!"
+        )
+        # if past states exist in the state vector, update them
+        if self.N > 0:
+            dX_past_temp = dX_past.reshape((self.N, 6))
+            dps = np.expand_dims(dX_past_temp[:, 3:6], axis=2) # (N, 3, 1)
+            dthetas = dX_past_temp[:, :3]
+            dRs = maths.vec_exponential_SO3_operation(dthetas) # (N, 3, 3)
+            # stack past states (Rs and ps) into an array
+            Rs_past = np.stack(self.si_Rs, axis=0)
+            ps_past = np.stack(self.si_ps, axis=0)
+            # update the past states with delta corrections
+            Rs_past_new = np.matmul(dRs, Rs_past)
+            ps_past_new = ps_past + dps
+            # reshape and store the corrected past states
+            N = Rs_past.shape[0]
+            self.si_Rs = np.split(Rs_past_new.reshape(N*3, 3), N, 0) # a list of N (3, 3) arrays
+            self.si_ps = np.split(ps_past_new.reshape(N*3, 1), N, 0) # a list of N (3, 1) arrays
         
+        # update the current state
+        dtheta = dX_evol[:3]
+        dR = maths.exponential_SO3_operation(dtheta)
+        self.s_R = dR.dot(self.s_R)
+        dv = dX_evol[3:6]
+        self.s_v = self.s_v + dv
+        dp = dX_evol[6:9]
+        self.s_p = self.s_p + dp
+        dbg = dX_evol[9:12]
+        self.s_bg = self.s_bg + dbg
+        dba = dX_evol[12:15]
+        self.s_ba = self.s_ba + dba
+    
+    def cal_corrections(self, target_state):
+        """
+        Return the error state vector between the current state and given target state.
+        NOTE: target_state is a State object.
+        """
+        assert target_state.N == self.N
+        dX = np.zeros((self.N*6+15, 1))
+
+        # update the corrections for past states
+        for i in range(len(self.si_Rs)):
+            # first append rotation correction of 3-dim
+            dX[6*i:6*i+3] = maths.logarithm_map_R(
+                target_state.si_Rs[i] @ (self.si_Rs[i].inverse())
+            )
+            # then append position correction of 3-dim
+            dX[6*i+3:6*i+6] = target_state.si_ps[i] - self.si_ps[i]
+        
+        # update the corrections for current state
+        dX[6*self.N:][:3, 0] = maths.logarithm_map_R(target_state.s_R @ self.s_R.T)
+        dX[6*self.N:][3:6, :] = target_state.s_v - self.s_v
+        dX[6*self.N:][6:9, :] = target_state.s_p - self.s_p
+        dX[6*self.N:][9:12, :] = target_state.s_bg - self.s_bg
+        dX[6*self.N:][12:15, :] = target_state.s_ba - self.s_ba
+
+        return dX
+    
+    def generate_unobservable_shift(self):
+        """
+        Return a dX element along certain unobservable directions.
+        """
+        assert self.N == 0
+        g = np.array([[0], [0], [1]])
+        dX = np.zeros((15, 4))
+
+        dX[0:3, [0]] = g
+        dX[3:6, [0]] = -maths.hat(self.s_v) @ g
+        dX[6:9, [0]] = -maths.hat(self.s_p) @ g
+        dX[6:9, 1:4] = np.eye(3)
+        return dX
+
+
+
+
+
 
 
 
--- utils/math_utils.py
+++ utils/math_utils.py
@@ -1,6 +1,15 @@
 from numba import jit
 import numpy as np
 
+def hat(v):
+    v = np.squeeze(v)
+    R = np.array([
+        [0, -v[2], v[1]], 
+        [v[2], 0, -v[0]], 
+        [-v[1], v[0], 0]
+    ])
+    return R
+
 @jit(nopython=True, parallel=False, cache=True)
 def exponential_SO3_operation(omega):
     """
@@ -30,6 +39,9 @@ def exponential_SO3_operation(omega):
     
     # Rodrigues' formula for the rotation matrix
     return c * np.identity(3) + (1 - c) * np.outer(axis, axis) + s * hat(axis)
+
+# vectorize the function of exponential_SO3_operation
+vec_exponential_SO3_operation = np.vectorize(exponential_SO3_operation, signature="(3)->(3,3)")
 
 
 @jit(nopython=True, parallel=False, cache=True)
@@ -118,3 +130,89 @@ def rot_2vec(a, b):
     
     return R_ba
  
+def cal_q_from_R(R):
+    """
+    Return a quaternion q of order (x,y,z,w) from a given rotation matrix R.
+    """
+    is_single = False
+    R = np.asarray(R, dtype=float)
+
+    if R.ndim not in [2, 3] or R.shape[-2:] != (3, 3):
+        raise ValueError(
+            "Expected `R` to have shape (3, 3) or "
+            "(N, 3, 3), got {}".format(R.shape)
+        )
+    # If a single R is given, convert it to 3D 1 x 3 x 3 R but
+    # set self._single to True so that we can return appropriate objects in
+    # the `to_...` methods
+    if R.shape == (3, 3):
+        R = R.reshape((1, 3, 3))
+        is_single = True
+    num_rotations = R.shape[0]
+
+    decision_R = np.empty((num_rotations, 4))
+    decision_R[:, :3] = R.diagonal(axis1=1, axis2=2)
+    decision_R[:, -1] = decision_R[:, :3].sum(axis=1)
+    choices = decision_R.argmax(axis=1)
+
+    quat = np.empty((num_rotations, 4))
+
+    ind = np.nonzero(choices != 3)[0]
+    i = choices[ind]
+    j = (i + 1) % 3
+    k = (j + 1) % 3
+
+    quat[ind, i] = 1 - decision_R[ind, -1] + 2 * R[ind, i, i]
+    quat[ind, j] = R[ind, j, i] + R[ind, i, j]
+    quat[ind, k] = R[ind, k, i] + R[ind, i, k]
+    quat[ind, 3] = R[ind, k, j] - R[ind, j, k]
+
+    ind = np.nonzero(choices == 3)[0]
+    quat[ind, 0] = R[ind, 2, 1] - R[ind, 1, 2]
+    quat[ind, 1] = R[ind, 0, 2] - R[ind, 2, 0]
+    quat[ind, 2] = R[ind, 1, 0] - R[ind, 0, 1]
+    quat[ind, 3] = 1 + decision_R[ind, -1]
+
+    quat /= np.linalg.norm(quat, axis=1)[:, None]
+
+    if is_single:
+        return quat[0]
+    else:
+        return quat
+
+def logarithm_map_R(R):
+    """
+    Compute the logarithm map of a rotation matrix R.
+    Return a Lie algebra (so(3)) representation, a 3D rotation vector.
+    """
+    # compute the quaternion as xyzw
+    q = cal_q_from_R(R)
+
+    # extract the scalar part and the vector part
+    q_w = q[-1]
+    q_vec = q[:3]
+
+    # compute the norm of the vector part
+    q_vec_norm = np.linalg.norm(q_vec)
+
+    # small threshold to prevent numerical instability
+    epsilon = 1e-7
+
+    # compute the logarithm map
+    if q_vec_norm < epsilon:
+        # when the q norm is very small, use a first-order approximation
+        q_w_sq = q_w * q_w
+        q_vec_norm_sq = q_vec_norm * q_vec_norm
+        # approximation for small angles
+        atn = 2.0/q_w - (2.0*q_vec_norm_sq) / (q_w*q_w_sq)
+    else:
+        if np.abs(q_w) < epsilon:
+            # handle the special case when q_w is close to zero
+            # this occurs when the rotation is close to 180 degrees
+            atn = np.pi / q_vec_norm if q_w > 0 else -np.pi / q_vec_norm
+        else:
+            # general case: use the atan2 function for better numerical stability
+            atn = 2.0 * np.arctan2(q_vec_norm, q_w) / q_vec_norm
+    # compute the final rotation vector (axis-angle)
+    tangent = atn * q_vec
+    return tangent
